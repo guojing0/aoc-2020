c51828456c8bb5d3a0da5471a12583768b13f33f
Jing Guo <dev.guoj@gmail.com>
2021-10-26 01:35:24 +0800

    Update 3.lisp

--- 3.lisp
+++ 3.lisp
@@ -4,9 +4,9 @@
 
 ;; A straight-forward way to complete first task
 (print (count t
-	      (loop for line in *map*  collect
-				       (eql #\# (elt line
-						     (rem (* 3 (position line *map*))
-							  *map-width*))))))
+	      (loop for line in *map* collect
+				      (eql #\# (elt line
+						    (rem (* 3 (position line *map*))
+							 *map-width*))))))
 
 ;; A more mathematical solution for the rest...
