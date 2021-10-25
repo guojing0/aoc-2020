63fdf8c20c84852562e89dbd1dcb330fccd3a0d5
Jing Guo <dev.guoj@gmail.com>
2021-10-26 01:33:27 +0800

    3

---
+++ 3.lisp
@@ -0,0 +1,12 @@
+(defparameter *map* (uiop:read-file-lines "day3.txt"))
+(defparameter *map-width* (length (car *map*)))
+(defparameter *map-length* (length *map*))
+
+;; A straight-forward way to complete first task
+(print (count t
+	      (loop for line in *map*  collect
+				       (eql #\# (elt line
+						     (rem (* 3 (position line *map*))
+				       *map-width*))))))
+
+;; A more mathematical solution for the rest...
