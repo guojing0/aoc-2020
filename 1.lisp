f8199d4ac72f5a39b5c3ff9cc2fc502b37d01e2b
Jing Guo <dev.guoj@gmail.com>
2021-10-26 00:22:13 +0800

    first and second lisp files

---
+++ 1.lisp
@@ -0,0 +1,16 @@
+(defparameter *numbers*
+  (mapcar #'parse-integer
+	  (uiop:read-file-lines "day1.txt")))
+
+(loop named top
+      for x in *numbers* do
+	(loop for y in *numbers*
+	      when (= 2020 (+ x y))
+		do (return-from top (print (* x y)))))
+
+(loop named top
+      for x in *numbers* do
+	(loop for y in *numbers* do
+	  (loop for z in *numbers*
+		when (= 2020 (+ x y z))
+		  do (return-from top (print (* x y z))))))
---
+++ 2.lisp
@@ -0,0 +1,30 @@
+(ql:quickload :split-sequence)
+(ql:quickload :alexandria)
+
+(print (count-if
+	(lambda (pswd-line)
+	  (destructuring-bind (range ch-colon pswd)
+	      (split-sequence:split-sequence #\Space pswd-line)
+	    (destructuring-bind (min-range max-range)
+		(split-sequence:split-sequence #\- range)
+	      (let ((ch (coerce (subseq ch-colon 0 1) 'character))
+		    (min-range (parse-integer min-range))
+		    (max-range (parse-integer max-range)))
+		(flet ((in-range (n min max)
+			 (and (>= n min) (<= n max))))
+		  (in-range (count ch pswd) min-range max-range))))))
+	(uiop:read-file-lines "day2.txt")))
+
+
+(print (count-if
+	(lambda (pswd-line)
+	  (destructuring-bind (range ch-colon pswd)
+	      (split-sequence:split-sequence #\Space pswd-line)
+	    (destructuring-bind (first-pos second-pos)
+		(split-sequence:split-sequence #\- range)
+	      (let ((ch (find-if #'alpha-char-p ch-colon))
+		    (first-pos (parse-integer first-pos))
+		    (second-pos (parse-integer second-pos)))
+		(alexandria:xor (eql ch (elt pswd (1- first-pos)))
+				(eql ch (elt pswd (1- second-pos))))))))
+	(uiop:read-file-lines "day2.txt")))
