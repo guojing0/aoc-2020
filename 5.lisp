b12cdd695659c795391640b947c4a8c38520ad8f
Jing Guo <dev.guoj@gmail.com>
2021-10-26 13:47:56 +0800

    5

---
+++ 5.lisp
@@ -0,0 +1,26 @@
+(defparameter *boarding-passes* (uiop:read-file-lines "day5.txt"))
+
+(defun modify (pass)
+  (nsubstitute #\1 #\B pass)
+  (nsubstitute #\0 #\F pass)
+  (nsubstitute #\1 #\R pass)
+  (nsubstitute #\0 #\L pass)
+  pass)
+
+(defun seat-id (mod-pass)
+  (+ (* 8 (parse-integer mod-pass :end 7 :radix 2))
+     (parse-integer mod-pass :start 7 :radix 2)))
+
+(print (loop for pass in *boarding-passes*
+	     maximize (seat-id (modify pass))))
+
+;; Part II
+
+(defparameter *mod-boarding-passes*
+  (loop for pass in *boarding-passes*
+	collect (seat-id (modify pass))))
+
+(print (loop for (x y) on (sort *mod-boarding-passes* #'<)
+	     while y
+	     when (= 2 (- y x))
+	       return (1+ x)))
