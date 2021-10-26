370b169a6ccf4f0145c3d31c79b8ab29ca1dd39c
Jing Guo <dev.guoj@gmail.com>
2021-10-26 14:49:16 +0800

    6

---
+++ 6.lisp
@@ -0,0 +1,18 @@
+(ql:quickload '("alexandria" "cl-ppcre"))
+
+(defparameter *answers*
+  (cl-ppcre:split "\\n\\n"
+		  (alexandria:read-file-into-string "day6.txt")))
+
+;; Part I
+
+(print (loop for ans in *answers*
+	     sum (length (remove-duplicates
+			  (cl-ppcre:regex-replace-all "\\n" ans "")))))
+
+;; Part II
+
+(print (loop for ans in *answers*
+	     sum (length (reduce #'intersection
+				 (mapcar (lambda (a) (coerce a 'list))
+					 (cl-ppcre:split "\\n" ans))))))
